e04762b9c5ee34d6a6c41b5eeea36d190a1f7cb7
rishhhh-dev <rishigupta92173@gmail.com>
2025-11-27 01:17:26 +0530

    feat:main file updated with new routers

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from .utils import models
 from .utils.database import engine
-from .users import router
+from .users import router as user_router
+from .category import router as category_router
+from .budgetrule import router as budget_router
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
-app.include_router(router.router)
+app.include_router(user_router.router)
+app.include_router(category_router.router)
+app.include_router(budget_router.router)
